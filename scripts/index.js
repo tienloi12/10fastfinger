35287eedae2d2cf5066ead1b1b025b91d2f5e3f6
Vu Tien Loi <loi.vt13104@sinhvien.hoasen.edu.vn>
2023-02-04 22:22:41 +0700

    them moi

---
+++ scripts/index.js
@@ -0,0 +1,18 @@
+let TIME_LIMIT = 30;
+let list_text = [ "the","quick","fox","jumped","over"
+            ,"to","be","for","not","to"
+            ,"that","is","the","form","imitation"
+            ,"color","gold","pen","code","laptop"
+            ,"phone","notebook","class","girl","boy"
+            ,"one","two","tree","three","four"
+            ,"five","six","seven","eight","nine"
+            ,"ten","hit"]
+let time_text = document.getElementById("curr_time");
+let error_text = document.getElementById("curr_errors");
+let input_text = document.getElementById("input");
+
+let timeLeft = TIME_LIMIT;
+let total_errors = 0;
+let errors = 0;
+let 
+
