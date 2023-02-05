1f6b03876f88edd26c3d9537e0370f3c7a1c6b1d
Vu Tien Loi <loi.vt13104@sinhvien.hoasen.edu.vn>
2023-02-05 22:10:30 +0700

    chua hoan thanh

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,5 @@
-let TIME_LIMIT = 30;
-let list_text = [ "the","quick","fox","jumped","over"
+
+let word_array = [ "the","quick","fox","jumped","over"
             ,"to","be","for","not","to"
             ,"that","is","the","form","imitation"
             ,"color","gold","pen","code","laptop"
@@ -7,12 +7,15 @@ let list_text = [ "the","quick","fox","jumped","over"
             ,"one","two","tree","three","four"
             ,"five","six","seven","eight","nine"
             ,"ten","hit"]
-let time_text = document.getElementById("curr_time");
-let error_text = document.getElementById("curr_errors");
-let input_text = document.getElementById("input");
+let count = 0;
+let inputText = document.getElementById("input-box");
+let score = document.getElementById("curr-score");
+let timer = document.getElementById("curr-time");
+let inputWord = document.getElementById("word");
+let result = document.getElementById("result");
 
-let timeLeft = TIME_LIMIT;
-let total_errors = 0;
-let errors = 0;
-let 
-
+function randomWord() {
+    let index = Math.floor(Math.random()*arrayWord.length)
+    return index
+     }
+    
