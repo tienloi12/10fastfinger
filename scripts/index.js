ca3f6006dbdd448182931857f9f3b3683fff13ed
Vu Tien Loi <loi.vt13104@sinhvien.hoasen.edu.vn>
2023-02-06 17:44:16 +0700

    them

--- scripts/index.js
+++ scripts/index.js
@@ -1,21 +1,38 @@
 
-let word_array = [ "the","quick","fox","jumped","over"
-            ,"to","be","for","not","to"
-            ,"that","is","the","form","imitation"
-            ,"color","gold","pen","code","laptop"
-            ,"phone","notebook","class","girl","boy"
-            ,"one","two","tree","three","four"
-            ,"five","six","seven","eight","nine"
-            ,"ten","hit"]
-let count = 0;
+let word_array = ["the", "quick", "fox", "jumped", "over"
+    , "to", "be", "for", "not", "to"
+    , "that", "is", "the", "form", "imitation"
+    , "color", "gold", "pen", "code", "laptop"
+    , "phone", "notebook", "class", "girl", "boy"
+    , "one", "two", "tree", "three", "four"
+    , "five", "six", "seven", "eight", "nine"
+    , "ten", "hit"]
 let inputText = document.getElementById("input-box");
-let score = document.getElementById("curr-score");
-let timer = document.getElementById("curr-time");
 let inputWord = document.getElementById("word");
 let result = document.getElementById("result");
 
 function randomWord() {
-    let index = Math.floor(Math.random()*arrayWord.length)
-    return index
-     }
-    
+    let index = Math.floor(Math.random() * word_array.length)
+    return index;
+}
+inputWord.innerHTML = word_array[randomWord()];
+function checkWord() {
+    if (inputText.value == inputWord.innerHTML) {
+        result.innerHTML = "Right";
+    }
+    else {
+        result.innerHTML = "Wrong";
+    }
+    inputText.value = ""
+    inputText.focus()
+
+}
+inputText.addEventListener("keypress", function (event) {
+    if (event.key === "Enter") {
+        checkWord();
+        randomWord();
+        inputWord.innerHTML = word_array[randomWord()];
+    }
+});
+
+
